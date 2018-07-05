800cf84ab356858083be38017ecf246a2ce1f020
Frédéric BISSON <zigazou@free.fr>
2018-07-05 08:16:19 +0200

    Test each link on the home page

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -1,11 +1,31 @@
 module Handler.HomeSpec (spec) where
 
+import           Data.Either                (fromRight)
+import qualified Data.Text                  as T
+import           Network.Wai.Test           (simpleBody)
 import           TestImport
+import           Yesod.Test.TransversingCSS (findAttributeBySelector)
+
+getAllLinks :: YesodExample site [Text]
+getAllLinks = withResponse $ \res -> do
+    let links = fromRight [] findAttributeBySelector (simpleBody res) "a" "href"
+    return $ T.concat <$> links
 
 spec :: Spec
 spec = withApp $
-    describe "Homepage" $
+    describe "Homepage" $ do
         it "loads the index and checks it looks right" $ do
             get HomeR
             statusIs 200
             htmlAnyContain "h1" "Bazasso"
+
+        it "checks all links" $ do
+            get HomeR
+            statusIs 200
+            links <- getAllLinks
+
+            forM_ links $ \oneLink -> do
+                get HomeR
+                statusIs 200
+                get oneLink
+                statusIs 200
