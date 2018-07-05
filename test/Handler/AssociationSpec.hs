f17f573047ad09c4a63d5390919f72d02192bd87
Frédéric BISSON <zigazou@free.fr>
2018-07-05 22:51:38 +0200

    Added Association tests

---
+++ test/Handler/AssociationSpec.hs
@@ -0,0 +1,19 @@
+module Handler.AssociationSpec (spec) where
+
+import           TestImport
+
+spec :: Spec
+spec = withApp $ do
+    describe "Expotec" $
+
+        it "gives a 200" $ do
+            get (NewAssociationR "W763007801")
+            statusIs 200
+            htmlAnyContain "h1" "EXPOTEC 103"
+
+    describe "Compagnie provisoire" $
+        it "gives a 200" $ do
+            get (OldAssociationR "601P0601014236")
+            statusIs 200
+            htmlAnyContain "h1" "COMPAGNIE PROVISOIRE"
+
